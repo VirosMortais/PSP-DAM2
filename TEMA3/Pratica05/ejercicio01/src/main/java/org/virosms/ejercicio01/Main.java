64c1a436c37e69244609b364b3c542de3a22321f
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2024-02-22 18:05:40 +0100

    Ejercicio 1 y 2 terminado

---
+++ TEMA3/Pratica05/ejercicio01/src/main/java/org/virosms/ejercicio01/Main.java
@@ -0,0 +1,8 @@
+package org.virosms.ejercicio01;
+
+public class Main {
+    public static void main(String[] args) {
+        Chat chat = new Chat();
+        chat.askForUrl();
+    }
+}
---
+++ TEMA3/Pratica05/ejercicio02/src/main/java/org/virosms/ejercicio02/Main.java
@@ -0,0 +1,8 @@
+package org.virosms.ejercicio02;
+
+public class Main {
+    public static void main(String[] args) {
+        Chat chat = new Chat();
+        chat.askForUrl();
+    }
+}
