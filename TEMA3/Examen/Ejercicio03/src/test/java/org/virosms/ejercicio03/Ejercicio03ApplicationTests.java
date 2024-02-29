68dd621cdb5556ac96331222f7442034024f3268
chaarrsan <charlestacodeouro@hotmail.com>
2024-02-29 17:39:30 +0100

    Terminado

---
+++ TEMA3/Examen/Ejercicio03/src/test/java/org/virosms/ejercicio03/Ejercicio03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.virosms.ejercicio03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
